4845519230c339431ec15efbad630b3bc6bd6725
veronichkasereda <118292754+veronichkasereda@users.noreply.github.com>
2023-04-13 20:47:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,83 @@
-import sys
+import telebot
+from telebot import types  # для указание типов
 
-import sys
-import sqlite3
-from PyQt5.Qt import *
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from Desine_demo import Ui_Dialog
+from config import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
-class MyWidget(QMainWindow, Ui_Dialog):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.on_cliced_button)
-        self.comboBox.currentTextChanged.connect(self.run)
-        self.comboBox_2.currentTextChanged.connect(self.on_combobox_func1)
-        self.comboBox.addItems(["Выбор предмета", "Планиметрия", "Тригонометрия"])
-        self.comboBox_2.addItems(["Выбор формулы"])
-
-    def run(self, text):
-        self.current_text = text
-        if self.current_text == "Планиметрия":
-            self.comboBox_2.addItems(
-                ["площадь прямоугольника", "площадь круга", "площадь трапеции",
-                                  "площадь ромба", "площадь параллелограмма", "площадь прямоугольного треугольника",
-                                  "площадь треугольника", "площадь квадрата"]
-                                 )
-        elif self.current_text == "Тригонометрия":
-            self.comboBox_2.addItems(
-                ["косинус", "синус", "тангенс", "котангенс"]
-            )
-
-    def on_combobox_func1(self, text1):
-        self.current_text1 = text1
-
-    def on_cliced_button(self, text1):
-        with sqlite3.connect("Geometry.db") as db:
-            cursor = db.cursor()
-            result = cursor.execute(
-                "SELECT picture FROM geometry WHERE name = ?", [self.current_text1]
-            ).fetchall()
-        self.get_picture(str(*result)[2:-3])
-
-    def get_picture(self, resultat):
-        self.title = "Image Viewer"
-        self.setWindowTitle(self.title)
-
-        label = QLabel(self)
-        pixmap = QPixmap(str(f"pictures\{resultat}"))
-        label.setPixmap(pixmap)
-        self.setCentralWidget(label)
-        self.resize(pixmap.width(), pixmap.height())
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Начать игру")
+    button2 = types.KeyboardButton("Есть вопросик")
+    button3 = types.KeyboardButton("Узнать правила игры")
+    markup.add(button1, button2, button3)
+    bot.send_message(message.chat.id,
+                     text="Привет, {0.first_name}! Я бот-помощник для проекта фото-квеста 'Назад в прошлое'".format(
+                         message.from_user), reply_markup=markup)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if (message.text == "Начать игру"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Задание 1")
+        button2 = types.KeyboardButton("Задание 2")
+        button3 = types.KeyboardButton("Задание 3")
+        home = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(button1, button2, button3, home)
+        bot.send_message(message.chat.id,
+                         text="Тебе предстоит выполнить 3 задания. Один или вместе с командой ты должен прийти в указанное место, сделать и отправить вашу общую фотографию. Ты можешь посмотреть все задания и выбрать сначала то, что больше тебе понравится, но будь очень внимателен при отправке своего ответа!",
+                         reply_markup=markup)
+    elif (message.text == "Узнать правила игры"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Начать игру")
+        home = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(button1, home)
+        bot.send_message(message.chat.id,
+                         text="Твоя задача - выполнять задания, которые я буду отправять. Приготовьтесь интересно, а главное - с пользой провести это время!)",
+                         reply_markup=markup)
+
+    elif (message.text == "Есть вопросик"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Начать игру")
+        home = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(button1, home)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+#в этих трех условиях бот отправляет человеку фоточки заданий с их текстом
+    elif (message.text == "Задание 1"):
+        img = open('pictures/first_picture.jpg', 'rb')
+        bot.send_photo(message.chat.id, img)
+        bot.send_message(message.chat.id, text="Круто! Что мы будем делать дальше?")
+
+    elif (message.text == "Задание 2"):
+        img = open('pictures/second_picture.jpg', 'rb')
+        bot.send_photo(message.chat.id, img)
+        bot.send_message(message.chat.id, text="Так держать!")
+
+
+    elif (message.text == "Задание 3"):
+        img = open('pictures/third_picture.jpg', 'rb')
+        bot.send_photo(message.chat.id, img)
+        bot.send_message(message.chat.id, text="Очень классно получилось!)")
+
+
+    elif (message.text == "Вернуться в главное меню"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Начать игру")
+        button2 = types.KeyboardButton("Есть вопросик")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+
+#тут в переменную picture мы кладем ссылку на фото, отправленное пользователем
+    elif ("Ответ на задание 1" in message.text):
+        picture = str(message.text)[19:]
+        print(picture)
+        bot.send_message(message.chat.id, text="Очень классно получилось!)")
+    else:
+        bot.send_message(message.chat.id,
+                         text="К сожалению, такой команды я не знаю. Может ты имел ввиду что-то другое?")
+
+
+bot.polling(none_stop=True)
